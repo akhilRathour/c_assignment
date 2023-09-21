4cb9673db985bea8beb79af40b17f4d973122a4c
akhilRathour <145379418+akhilRathour@users.noreply.github.com>
2023-09-21 11:41:44 +0530

    Update q4_1.c

--- w4_1.c
+++ w4_1.c
@@ -12,22 +12,20 @@ int main() {
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        //  spaces before
+       
         for (int j = 1; j <= n - i; j++) {
             printf(" ");
         }
 
-        // Print increasing numbers
+       
         for (int j = 1; j <= i; j++) {
             printf("%d", j);
         }
 
-        // Print decreasing numbers
+        
         for (int j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
-
-        // Move to the next line
         printf("\n");
     }
 
