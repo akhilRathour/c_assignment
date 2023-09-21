88850f0d433fb2f1ae89998ed40f72998dad23cf
akhilRathour <145379418+akhilRathour@users.noreply.github.com>
2023-09-21 18:42:24 +0530

    Update q2_4.c

--- q2_4.c
+++ q2_4.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
 int main() {
-    int a ;
-    int b ;
-    int c ;
-    printf("enter numbers\n");
-    scanf("%d %d %d",&a,&b,&c);
+    int a, b, c, d;
+    printf("Enter four numbers: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    
     int max_num, min_num;
-
+    
+   
     max_num = min_num = a;
+    
 
     if (b > max_num) {
         max_num = b;
-    }
-    else if (b < min_num) {
+    } else if (b < min_num) {
         min_num = b;
     }
-
+    
    
     if (c > max_num) {
         max_num = c;
-    }
-    else if (c < min_num) {
+    } else if (c < min_num) {
         min_num = c;
     }
-
+    
+    // Compare d with max and min
+    if (d > max_num) {
+        max_num = d;
+    } else if (d < min_num) {
+        min_num = d;
+    }
+    
     printf("Maximum number: %d\n", max_num);
     printf("Minimum number: %d\n", min_num);
-
+    
     return 0;
 }
